50e0bc4b940e328b40fa9e706aaa370ed52d452c
prxg8 <68958486+prxg8@users.noreply.github.com>
2020-08-04 09:58:26 -0500

    Update voidAreas.py

--- voidAreas.py
+++ voidAreas.py
@@ -40,5 +40,18 @@ plt.imshow(labels)
 plt.show()
 
 areas=[np.count_nonzero(labels==n)*pixel_area for n in np.unique(labels)]
+github add
+M= cv2.moments(labels)
+cx=int(M['m10']/M['m00'])
+cy=int(M['m01']/M['m00'])
+
+center=(cx,cy)
+cv2.putText(img2,"A:{0:2.1f}".format(areas),center,
+            cv2.FONT_HERSHEY_COMPLEX_SMALL,1.3,(255,0,0),3)
+
+
+plt.show(areas)
+print(areas)
+
 
 print(areas)

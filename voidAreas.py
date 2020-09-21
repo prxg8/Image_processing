1c923c99382eb3d902c761c09604732bb6b9cf8d
Todd Sparks <toddesparks@gmail.com>
2020-09-20 21:29:03 -0500

    fixed an incorrect assumption that a threshold of a edge finding filter means anything

--- voidAreas.py
+++ voidAreas.py
@@ -16,7 +16,8 @@ import skimage.segmentation
 from PIL import ImageFilter
 
 
-img = Image.open (r"C:/Users/rpras/Desktop/missouri s n t/Image Analysis images/Questek/spec 3 10x -1.tiff")  # open the image
+#img = Image.open (r"C:/Users/rpras/Desktop/missouri s n t/Image Analysis images/Questek/spec 3 10x -1.tiff")  # open the image
+img=Image.open(sys.argv[1])
 img2 = img.convert('L')
  # make sure its greyscale
 
@@ -31,26 +32,23 @@ threshold=(values[valleys[0]]+np.mean(img2))/2 # try the value of the first vall
 print(threshold)
 
 img3 = img2.filter(ImageFilter.FIND_EDGES)
-img3.show()
+#img3.show()
 
-thresh=img3.point(lambda p: p<threshold and 255) # switch to threshold in PIL
+thresh=img2.point(lambda p: p<threshold and 255) # switch to threshold in PIL
 
 thresharr=np.array(thresh) # convert to a numpy array
-
 # next I'm going to clean up the image a bit
 n_cycles=2 # increase this for more "cleaning"
 
-edgeThresh=n_cycles*3
 edge=np.zeros_like(thresh) # mask for detecting if the region is on the edge
-edge[:n_cycles,:]=1
-edge[-n_cycles:,:]=1
-edge[:,:n_cycles]=1
-edge[:,-n_cycles:]=1
-
+edge[:n_cycles*2,:]=1
+edge[-n_cycles*2:,:]=1
+edge[:,:n_cycles*2]=1
+edge[:,-n_cycles*2:]=1
 
 thresharr=binary_erosion(thresharr,iterations=n_cycles) # this shrinks all the white objects.  tiny dots will go away
-thresharr=binary_dilation(thresharr,iterations=n_cycles*3) # expand objects to try to close up holes
-thresharr=binary_erosion(thresharr,iterations=n_cycles*2) # shrink back to the size you started with
+thresharr=binary_dilation(thresharr,iterations=n_cycles+1) # expand objects to try to close up holes
+thresharr=binary_erosion(thresharr,iterations=1) # shrink back to the size you started with
 
 labels= measure.label(thresharr, connectivity=2, background=0) # label the areas
 edgeDetect=labels*edge
@@ -58,7 +56,7 @@ edgeDetect=labels*edge
 testRegions=np.unique(labels)[1:]
 regions=[]
 for R in testRegions:
-	if not(np.any(edgeDetect==R)):
+	if not(np.any(edgeDetect*(labels==R))):
 		regions.append(R)
 
 areas=[np.count_nonzero(labels==n)*pixel_area for n in regions] # area of each labeled, nonbackground reagion
